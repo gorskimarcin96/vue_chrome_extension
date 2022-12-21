6f53703518b492e0db7cbdb94073a7d4864258a5
Marcin Górski <gorskimarcin96@gmail.com>
2022-12-21 14:56:22 +0100

    Added cache for axios.

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,13 @@
 import axios from 'axios'
+import wrapper from 'axios-cache-plugin'
 
-const api = axios.create({
+const clientAxios = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL + '/api',
+})
+
+const api = wrapper(clientAxios, {
+    maxCacheSize: 100,
+    ttl: 600000,
 })
 
 export default api;
