fb2d699020dffa40b1435c1d620c14790968edbc
Marcin Górski <gorskimarcin96@gmail.com>
2024-05-23 21:52:24 +0200

    Created logout listener.

--- src/api/mgorski/api.ts
+++ src/api/mgorski/api.ts
@@ -1,8 +1,15 @@
-import axios from "axios";
+import axios, {AxiosError} from "axios";
+import {notify} from "../../utils/notification";
 
 const api = axios.create({
     // @ts-ignore
     baseURL: import.meta.env.VITE_BACKEND_URL + '/api',
 })
+
+api.interceptors.response.use((response) => response, (error: AxiosError) => {
+    notify(error.toString());
+
+    throw error;
+});
 
 export default api;
--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,23 @@ import './assets/main.css'
 import {library} from '@fortawesome/fontawesome-svg-core'
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {faClock, faFile, faMessage, faRandom} from '@fortawesome/free-solid-svg-icons'
+import {ComponentPublicInstance} from "@vue/runtime-core"
 
 library.add(faFile)
 library.add(faMessage)
 library.add(faClock)
 library.add(faRandom)
 
-createApp(App)
+let app = createApp(App);
+
+app.config.errorHandler = (err: unknown, instance: ComponentPublicInstance | null, info: string) => {
+    if (err?.response?.status === 401 && instance) {
+        instance.$emit('user-data', null)
+    } else {
+        throw err;
+    }
+};
+
+app
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
