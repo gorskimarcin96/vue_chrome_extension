832ee544147f41801d177f5e7f3c550039c5a524
Marcin Górski <gorskimarcin96@gmail.com>
2025-03-25 13:18:09 +0100

    Removed storing ips.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,9 +4,6 @@ import {defineComponent} from "vue";
 import {User} from "@/models/User";
 import ip from "../api/ipify/ip";
 import {notify} from "@/utils/notification";
-import {IP} from "@/models/IP";
-import storage from "@/storage/storage";
-import moment from "moment/moment";
 import apiNju from "../api/mgorski/nju";
 import {Nju} from "@/models/Nju";
 
@@ -19,7 +16,6 @@ export default defineComponent({
       loadedData: false,
       isShowHistoryIp: false,
       ip: "",
-      ips: [] as IP[],
       isDevelopmentMode: false,
       njuData: null as Nju
     }
@@ -29,19 +25,6 @@ export default defineComponent({
     async copyIP(ip: string) {
       navigator.clipboard.writeText(ip.trim());
       notify('Added IP "' + ip.trim() + '" to clipboard.');
-    },
-    updateIPinStorage() {
-      for (const ip of this.ips) {
-        if (ip.ip === this.ip) {
-          return;
-        }
-      }
-
-      storage.addIP(this.ip);
-      this.ips.push({ip: ip, date: Date.now()})
-    },
-    timestampToString(date: number): string {
-      return moment(date).format('YYYY-MM-DD HH:mm')
     },
     showHistoryIp() {
       this.isShowHistoryIp = true;
@@ -53,8 +36,6 @@ export default defineComponent({
   created: async function () {
     this.isDevelopmentMode = process.env.NODE_ENV === 'development';
     this.ip = await ip.get();
-    this.ips = await storage.getIPs() ?? [];
-    this.updateIPinStorage();
     await this.fetchNjuData();
     this.loadedData = true;
   }
@@ -68,19 +49,8 @@ export default defineComponent({
       You are logged in as <span class="text-success fw-semibold">{{ user.email }}</span>.
     </span>
     <span v-if="!user || !user.token" class="badge bg-warning text-dark">You are not logged.</span>
-    <div class="max-height-100px overflow-auto">
-      <table class="ms-auto my-1" v-if="isShowHistoryIp">
-        <tbody>
-        <tr v-for="historyIP in ips">
-          <td class="px-2">{{ timestampToString(historyIP.date) }}</td>
-          <td class="fw-bolder" @click="copyIP(ip)">{{ historyIP.ip }}</td>
-        </tr>
-        </tbody>
-      </table>
-    </div>
     <div>
       <span v-if="ip" @click="copyIP(ip)">Your IP: <span class="fw-bolder">{{ ip }}</span></span>
-      <span v-if="!isShowHistoryIp" @click="showHistoryIp" class="link-success cursor-pointer"> - show IP history</span>
     </div>
     <span class="badge bg-warning text-dark" v-if="njuData">
       NJU {{ njuData.usedNet }}GB / {{ njuData.totalNet }}GB
